ec3b34850c41bb49aa6ac46c630ae73981e42cc8
joey906 <jtf1owataeba2ymi@gmail.com>
2022-03-07 23:29:27 +0900

    addRequest

--- forest/app/Http/Controllers/ForestController.php
+++ forest/app/Http/Controllers/ForestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Forest;
+use App\Http\Requests\ForestRequest;
 
 class ForestController extends Controller
 {
@@ -33,5 +34,34 @@ class ForestController extends Controller
         }
         
         return view('forest.detail',['forest' => $forest]);
+    }
+    
+    /**
+     * ブログ登録画面を表示
+     * @return view
+     */
+    public function showCreate() {
+        return view('forest.form');
+    }
+
+    /**
+     * ブログを登録する
+     * @return view
+     */
+    public function exeStore(ForestRequest $request) {
+        $inputs = $request->all();
+
+        \DB::beginTransaction();
+        try {
+            Forest::create($inputs);
+            \DB::commit();
+        } catch(\Throwable $e)
+         {
+             \DB::rollback();
+             abort(500);
+        }
+        
+        \Session::flash('err_msg', 'ブログを登録しました');
+        return redirect(route('top'));
     }
 }
--- forest/routes/web.php
+++ forest/routes/web.php
@@ -16,7 +16,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-
+//Forest一覧表示
 Route::get('/', 'ForestController@top')->name('top');
 
+//Forest登録画面表示
+Route::get('/forest/create', 'ForestController@showCreate')->name('create');
+
+//Forest登録
+Route::post('/forest/store', 'ForestController@exeStore')->name('store');
+
+//Forest詳細表示
 Route::get('/forest/{id}', 'ForestController@showDetail')->name('show');
